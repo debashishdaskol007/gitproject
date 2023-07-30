6d1c048a42ddefab66d1b4fb489564f6d27c8f55
Debashish Das <debashishdas.kol007@gmail.com>
2023-07-30 08:33:07 +0000

    first commit

---
+++ webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["com/gitproject/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"com/gitproject/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
